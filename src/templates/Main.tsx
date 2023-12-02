ec4eb2e6bf96ccaac9773e5a50ae4fecf6c5b04b
Juun <blacklily7513@gmail.com>
2023-12-02 20:53:13 +0900

    lint --fix applied

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -1,7 +1,4 @@
-import Link from 'next/link';
 import type { ReactNode } from 'react';
-
-import { AppConfig } from '@/utils/AppConfig';
 
 type IMainProps = {
   meta?: ReactNode;
@@ -12,6 +9,6 @@ const Main = (props: IMainProps) => {
   <div className="w-full px-1 text-gray-700 antialiased">
     {props.meta}
 
-    <div className="mx-auto max-w-screen-md"></div>
+    <div className="mx-auto max-w-screen-md" />
   </div>;
 };
