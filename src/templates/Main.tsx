cbbad6bba7a5984bf64cff554378be0e4ae8b706
Juun <blacklily7513@gmail.com>
2023-12-02 11:44:01 +0900

    setting up mains

---
+++ src/templates/Main.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import type { ReactNode } from "react";
+
+import { AppConfig } from "@/utils/AppConfig";
+
+type IMainProps = {
+  meta?: ReactNode;
+  children: ReactNode;
+};
+
+const Main = (props: IMainProps) => {
+  <div className="w-full px-1 text-gray-700 antialiased">
+    {props.meta}
+
+    <div className="mx-auto max-w-screen-md">
+      
+    </div>
+  </div>
+}
