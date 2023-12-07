36cabb5816b78beefb578291dd6859713acda639
Juun <blacklily7513@gmail.com>
2023-12-07 15:38:34 +0900

    fix(Env.mjs/package.json): minor fixes for eslint
    minor fixes

--- src/libs/Env.mjs
+++ src/libs/Env.mjs
@@ -1,6 +1,6 @@
-/* eslint-disable import/prefer-default-export */
+/* eslint-disable import/prefer-default-export, import/no-extraneous-dependencies */
 import { createEnv } from '@t3-oss/env-nextjs';
-import { z } from 'zod';
+// import { z } from 'zod';
 
 export const Env = createEnv({
   server: {},
