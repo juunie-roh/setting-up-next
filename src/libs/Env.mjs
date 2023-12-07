ce79021d47fe0c5aa86b6c8f064cd3b2096f8c82
Juun <blacklily7513@gmail.com>
2023-12-07 17:15:28 +0900

    style(Env.mjs): discard one of the eslint-disable option

--- src/libs/Env.mjs
+++ src/libs/Env.mjs
@@ -1,4 +1,4 @@
-/* eslint-disable import/prefer-default-export, import/no-extraneous-dependencies */
+/* eslint-disable import/prefer-default-export */
 import { createEnv } from '@t3-oss/env-nextjs';
 // import { z } from 'zod';
 
