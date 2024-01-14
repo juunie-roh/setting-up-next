1687886008783a1939272bfbd2d98d99bcbb11c0
Juun <blacklily7513@gmail.com>
2024-01-14 19:47:34 +0900

    build: unused variable removed

--- src/api/100days.ts
+++ src/api/100days.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 
 const filename = '/100days/index.html';
-export default async function api(req: any, res: any) {
+export default async function api(res: any) {
   res.setHeader('Content-Type', 'text/html; charset=utf-8');
   res.write(await fs.readFileSync(filename, 'utf-8'));
   res.end();
