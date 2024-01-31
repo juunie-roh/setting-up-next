1a50351a643125502fbde6507b796d3b6d458ff4
Juun <blacklily7513@gmail.com>
2024-01-31 17:46:08 +0900

    refactor: style adjustments

--- src/pages/techrecord/gltf-loader-example.tsx
+++ src/pages/techrecord/gltf-loader-example.tsx
@@ -37,15 +37,15 @@ const GLTFLoaderExample = () => {
         </Canvas>
       </ThreeCanvas>
       <DefaultSection>
-        <p>
+        <p className="mt-4">
           onPointerOver/Out 및 onClick(log) 이벤트 적용 <br />
           커서가 모델 위에서 움직이는데도 깜빡이는 현상 발생 <br />
           Property로 설정하면 커서 인식을 mesh 단위로 진행하는 것으로 추정됨
           <br />
           raycaster로 pointer 인식을 직접 구현했을 때는 깜빡이는 현상이 없었음
           <br />
-          raycaster와 intersect한 mesh가 포함된 group 전체에 isIntersected:{' '}
-          boolean 속성을 부여해서 group 별로 pointer를 인식하는 방식이었음
+          raycaster와 intersect한 mesh가 포함된 group 자체를 INTERSECTED
+          object로 관리해서 group 별로 pointer를 인식하는 방식이었음
           <br />
           목표 group을 array에 저장하여 원하는 group만 intersected 상호작용
           가능하다는 점이 있음
