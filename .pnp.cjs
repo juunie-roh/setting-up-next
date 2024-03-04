b81c79cacb6b164c63058caadf6595f805a8773c
Juun <blacklily7513@gmail.com>
2024-03-04 14:51:10 +0900

    fix(build fix): package dependencies fixed

--- .pnp.cjs
+++ .pnp.cjs
@@ -17147,13 +17147,6 @@ const RAW_RUNTIME_STATE =
       }]\
     ]],\
     ["ip", [\
-      ["npm:2.0.0", {\
-        "packageLocation": "./.yarn/cache/ip-npm-2.0.0-204facb3cc-8d186cc558.zip/node_modules/ip/",\
-        "packageDependencies": [\
-          ["ip", "npm:2.0.0"]\
-        ],\
-        "linkType": "HARD"\
-      }],\
       ["npm:2.0.1", {\
         "packageLocation": "./.yarn/cache/ip-npm-2.0.1-ee48691f03-cab8eb3e88.zip/node_modules/ip/",\
         "packageDependencies": [\
@@ -24852,7 +24845,7 @@ const RAW_RUNTIME_STATE =
         "packageLocation": "./.yarn/cache/socks-npm-2.7.1-17f2b53052-43f69dbc9f.zip/node_modules/socks/",\
         "packageDependencies": [\
           ["socks", "npm:2.7.1"],\
-          ["ip", "npm:2.0.0"],\
+          ["ip", "npm:2.0.1"],\
           ["smart-buffer", "npm:4.2.0"]\
         ],\
         "linkType": "HARD"\
